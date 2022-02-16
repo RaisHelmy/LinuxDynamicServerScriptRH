a89a02d1e86debde0fd2615e31ed9a157238cf45
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-02-16 22:23:23 +0800

    GetID and cronID
    Get ID:
    1. get ipv6 address
    2. Update address to psql db
    
    CronID
    1.  Run thing every 5 minit.
    (for test 10 second was used, you may change this to 300 second as needed.

--- GetID.sh
+++ GetID.sh
@@ -1,1 +1,10 @@
-echo "Hello World"
+greeting="Assalamualaikum and Hi!"
+user=$(whoami)
+day=$(date +%A)
+echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
+echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
+ipv6addr=$(ip addr show dev eth0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d') #ipv6 command
+echo "Your PC Name is: $(hostname)"
+echo "Your ipv6 address is: $ipv6addr"
+#psql -U postgres -d database_name -c "UPDATE FROM db_ip_info SET db_ipv6_addr WHERE server_id = $(hostname)"  <--- Use this psql command to update ur ip to db
+
---
+++ cronID.sh
@@ -0,0 +1,6 @@
+#This will run GetID.sh every 5 minute
+while true  
+do  
+  echo "Run every 10 second"
+  sleep 10 #Change this to 300 to run every 5min  
+done
