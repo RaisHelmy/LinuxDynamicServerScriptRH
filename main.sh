998e6a72235b4ddc34ffa858cbb134a33c28cb02
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-02-17 00:32:13 +0800

    main file
    User can just press input ABCD to run the script

---
+++ main.sh
@@ -0,0 +1,23 @@
+pwd=$(pwd)
+echo 'Press key below for operation'
+echo '      A: systemdGetID    note: One click solution'
+echo '      B: installCron     note: Change path and cron tab solution'
+echo '      C: getID           note: Just to get ID, not necessary or ignore'
+echo '      D: cronID          note: Just to run operation in loop, not necessary or ignore'
+read input
+case $input in
+    'A')
+    $pwd/systemdGetID.sh 
+     ;;
+    'B')
+    $pwd/installCron.sh
+     ;;
+    'C')
+    $pwd/GetID.sh
+     ;;
+    'D')
+    $pwd//cronID.sh
+    ;;
+    *)
+    exit 1
+esac
